965225a1cfcbc83ce67d980ebc6b07fcfe816f69
Nicholas Igunbor <nickoklaus5@gmail.com>
2022-08-28 06:29:38 -0700

    <"tuples">

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,13 +2,10 @@
 def print_matrix_integer(matrix=[[]]):
     "prints a matrix of integers"
 
-    row_len = len(matrix)
-    col_len = len(matrix[0])
-
-    for i in range(row_len):
-        for j in range(col_len):
-            if j != col_len -1:
-                print("{:d}".format(matrix[i][j], end=' ')
+    for row in matrix:
+        for i in row:
+            if i != row[-1]:
+                print("{:d}".format(i), end="")
             else:
-                print("{:d}".format(matrix[i][j], end='')
-        print("")
+                print("{:d}".format(i), end="")
+        print()
