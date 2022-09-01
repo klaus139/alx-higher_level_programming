14ccc1519bab6dbe58681731f9367077ea5c3fea
Nicholas Igunbor <nickoklaus5@gmail.com>
2022-09-01 08:49:46 -0700

    <"dictionary">

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    "replaces or adds key/value in a dictionary"
-
-
-    a_dictionary[key] = value
-    return (a_dictionary)
+        """Replace or add key/value pairs in a dictionary."""
+        a_dictionary[key] = value
+        return (a_dictionary)
