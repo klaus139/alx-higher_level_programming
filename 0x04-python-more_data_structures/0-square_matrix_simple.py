177e96f8f36ebb4b1d10ad2e86d9b81e4bededba
Nicholas Igunbor <nickoklaus5@gmail.com>
2022-09-01 07:09:10 -0700

    <"dictionary">

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     "computes the square value of all integers of a matrix"
-    [list(map(lambda x: x ** 2, row)) for row in matrix])
+    return ([list(map(lambda x: x ** 2, row)) for row in matrix])
